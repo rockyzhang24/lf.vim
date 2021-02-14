rockyzhang24/lf.vim

commit 40d04c124be577276e04f58aae013b16d1b90d17

Fix for breaking change in vim-floaterm
Fixes #23.

plugin/lf.vim
@@ -54,22 +54,13 @@ function! LfCallback(lf_tmpfile, lastdir_tmpfile, ...) abort
         call floaterm#window#hide(bufnr('%'))
       endif
       let locations = []
-      if edit_cmd != 'default'
-        for filename in filenames
-          let dict = {'filename': fnamemodify(filename, ':p')}
-
-          call add(locations, dict)
-        endfor
-        call floaterm#util#open(s:edit_cmd, locations)
-        unlet s:edit_cmd
-      else
-        for filename in filenames
-          let dict = {'filename': fnamemodify(filename, ':p')}
-
-          call add(locations, dict)
-        endfor
-        call floaterm#util#open(g:floaterm_open_command, locations)
-      endif
+      let floaterm_opener = edit_cmd != 'default' ? s:edit_cmd : g:floaterm_opener
+      for filename in filenames
+        let dict = {'filename': fnamemodify(filename, ':p')}
+        call add(locations, dict)
+      endfor
+      call floaterm#util#open(locations, floaterm_opener)
+      unlet s:edit_cmd
     endif
   endif
 endfunction

commit c80801760eeacb6fa9fa231408a8723d6a183262

Fix issue with vanilla `FloatermNew lf`
Issue introduced in 40d04c1.

Fixes #25.

plugin/lf.vim
@@ -60,7 +60,7 @@ function! LfCallback(lf_tmpfile, lastdir_tmpfile, ...) abort
         call add(locations, dict)
       endfor
       call floaterm#util#open(locations, floaterm_opener)
-      unlet s:edit_cmd
+      unlet! s:edit_cmd
     endif
   endif
 endfunction